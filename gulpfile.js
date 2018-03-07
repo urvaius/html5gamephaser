57034024dcac7f20c4a2b94aa6eb24c722fcef0b
urvaius <urvaius@gmail.com>
2018-03-07 13:44:39 -0800

    got the base tutorial working with my dev environment.  for looking at

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp'),
     connect = require('gulp-connect'),
-    open = require('gulp-open');
+    open = require('gulp-open'),
+    clean = require('gulp-clean');
 
 var config = {
     port:6005,
@@ -9,7 +10,8 @@ var config = {
         html: './src/*.html',
         js: './src/**/*.js',
         dist: './dist',
-        images: './src/assets/*.png'
+        images: './src/assets/*.png',
+        mainJs: './src/main.js'
     }
 };
 gulp.task('connect', function() {
@@ -20,8 +22,13 @@ gulp.task('connect', function() {
         livereload: true
     });
 });
+gulp.task('clean', function() {
+    return gulp.src(config.paths.dist)
+    .pipe(clean());
+});
 //opens index.html at port
 gulp.task('open', ['connect'], function() {
+    //gulp.src('dist/part1.html')
     gulp.src('dist/index.html')
     .pipe(open({ uri: config.devBaseUrl + ':' + config.port + '/'}));
 });
@@ -31,15 +38,15 @@ gulp.task('html', function() {
     .pipe(gulp.dest(config.paths.dist))
     .pipe(connect.reload());
 });
-gulp.task('images', function() {
+gulp.task('images', ['html'], function() {
     gulp.src(config.paths.images)
-    .pipe(gulp.dest(config.paths.dist))
+    .pipe(gulp.dest(config.paths.dist + '/images'))
     .pipe(connect.reload());
 });
 
 gulp.task('watch', function(){
     gulp.watch(config.paths.html, ['html']);
-    gulp.watch(config.paths.images,['png']);
+    gulp.watch(config.paths.images,['images']);
     gulp.watch(config.paths.js, ['js', 'lint']);
 });
-gulp.task('default', ['html', 'open', 'watch', 'images']);
+gulp.task('default', ['images', 'open', 'watch' ]);
